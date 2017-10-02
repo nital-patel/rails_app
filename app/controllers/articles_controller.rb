a176124486020a27440bae71ff86668fafc10c67
Nital Patel <patelnital36@gmail.com>
2017-10-02 16:13:30 -0400

    Add create

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,35 @@
+class ArticlesController < ApplicationController
+def index
+    @articles = Article.all
+  end
+
+def edit
+  @article = Article.find(params[:id])
+end
+
+   def create
+   @article = Article.new
+
+    if @article.save
+  redirect_to @article
+  else
+    render 'new'
+  end
+end
+
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  end
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+end
