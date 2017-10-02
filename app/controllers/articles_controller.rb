f42140f92d99e3349ddaf4dd704bf60ec791ea9a
Nital Patel <patelnital36@gmail.com>
2017-10-02 16:54:27 -0400

    Add delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,31 +1,7 @@
 class ArticlesController < ApplicationController
-def index
+  def index
     @articles = Article.all
   end
-
-def edit
-  @article = Article.find(params[:id])
-end
-
-   def create
-   @article = Article.new(article_params)
-
-    if @article.save
-  redirect_to @article
-  else
-    render 'new'
-  end
-end
-
-def update
-  @article = Article.find(params[:id])
-
-  if @article.update(article_params)
-    redirect_to @article
-  else
-    render 'edit'
-  end
-end
 
   def show
     @article = Article.find(params[:id])
@@ -35,10 +11,39 @@ end
     @article = Article.new
   end
 
-
-  private
-  def article_params
-    params.require(:article).permit(:title, :text)
+  def edit
+    @article = Article.find(params[:id])
   end
 
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
